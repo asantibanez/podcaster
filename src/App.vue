763d888f58fb2dd6b13fe34310c06525b3bf871c
asantibanez <santibanez.andres@gmail.com>
2019-03-13 15:42:35 -0500

    Added speech animation and recognition. Added recording

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
     <div id="app">
 
+        <span class="text-xl" v-show="isSelfSpeaking">Speaking...</span>
+        <span class="text-xl" v-show="!isSelfSpeaking">Not Speaking</span>
+        <lottie :options="defaultOptions" :height="200" :width="200" v-on:animCreated="handleAnimation"/>
+
+        <br>
+        <br>
+
         <div class="flex flex-row">
-            <video class="flex-1" ref="video" id="video" controls></video>
-            <video class="flex-1" ref="partner" id="partner" controls></video>
+            <audio class="flex-1" ref="self" id="self" controls></audio>
+            <audio class="flex-1" ref="partner" id="partner" controls></audio>
         </div>
 
         <br>
@@ -11,43 +18,74 @@
 
         <input type="text" v-model="host" class="border"/>
         <input type="text" v-model="port" class="border"/>
+
         <br>
         <br>
 
         <input type="text" v-model="peerId" class="border"/>
         <button @click="connect" class="border bg-gray p-2 rounded ml-2">Connect</button>
+
         <br>
         <br>
 
         <input type="text" v-model="invitePeerId" class="border"/>
         <button @click="invite" class="border bg-gray p-2 rounded ml-2">Invite</button>
+
+        <br>
+        <br>
+
+        Recorded chunks: {{ audioChunks.length }}
+
+        <button @click="startRecording" class="border bg-gray p-2 rounded ml-2">Start Recording</button>
+        <button @click="stopRecording" class="border bg-gray p-2 rounded ml-2">Stop Recording</button>
+        <audio ref="recording" id="recording" controls></audio>
+
     </div>
 </template>
 
 <script>
+    import Lottie from 'vue-lottie';
     import Peer from 'peerjs'
+    import Hark from 'hark'
+    import * as animationData from './assets/sound-bars-animation.json';
 
     export default {
         name: 'app',
+
+        components: {
+            'lottie': Lottie
+        },
 
         data() {
             return {
                 host: "23.239.12.93",
                 port: 3000,
 
-                video: {},
+                self: {},
                 partner: {},
 
                 peerId: "",
                 invitePeerId: "",
                 peer: null,
                 mediaStream: null,
+
+                recorder: null,
+                audioChunks: [],
+                isSelfSpeaking: false,
+
+                defaultOptions: {
+                    animationData: animationData.default
+                },
+                animationSpeed: 1,
+                animation: null,
             }
         },
 
         mounted() {
-            this.video = this.$refs.video;
+            this.self = this.$refs.self;
             this.partner = this.$refs.partner;
+            this.recording = this.$refs.recording;
+
             this.getUserVideo()
         },
 
@@ -73,17 +111,53 @@
             },
 
             getUserVideo() {
-                navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+                navigator.mediaDevices.getUserMedia({audio: true, video: false})
+                    .then(stream => {
+                        this.mediaStream = stream;
+                        this.self.srcObject = this.mediaStream;
+                        this.self.muted = true;
+                        this.self.play();
 
-                navigator.getUserMedia({audio: true, video: true}, stream => {
-                    this.mediaStream = stream;
-                    this.video.srcObject = this.mediaStream;
-                    this.video.muted = true;
-                    this.video.play();
-                }, () => {
-                    alert("Error! Make sure to click allow when asked for permission by the browser");
-                });
-            }
+                        let options = {};
+                        const speechRecognition = Hark(this.mediaStream, options);
+                        speechRecognition.on('speaking', () => {
+                            this.isSelfSpeaking = true;
+                            this.animation.play();
+                        });
+                        speechRecognition.on('stopped_speaking', () => {
+                            this.isSelfSpeaking = false;
+                            this.animation.stop();
+                        });
+
+                    }).catch(() => {
+                        alert("Error! Make sure to click allow when asked for permission by the browser");
+                    });
+            },
+
+            startRecording() {
+                this.audioChunks = [];
+
+                this.recorder = new MediaRecorder(this.mediaStream);
+                this.recorder.ondataavailable = event => this.audioChunks.push(event.data);
+                this.recorder.onstop = () => this.processAudio();
+
+                this.recorder.start(1000);
+            },
+
+            stopRecording() {
+                this.recorder.stop();
+
+            },
+
+            processAudio() {
+                let audioBlob = new Blob(this.audioChunks);
+                this.recording.src = URL.createObjectURL(audioBlob);
+            },
+
+            handleAnimation: function(animation) {
+                this.animation = animation;
+                this.animation.stop();
+            },
         }
     }
 </script>
